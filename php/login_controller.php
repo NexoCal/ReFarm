c11b50413172a4db102b25c1f47401b447fed61e
rmdhakbar <ramadhaniakbar991@gmail.com>
2024-06-29 13:38:51 +0700

    update logijn controller

--- php/login_controller.php
+++ php/login_controller.php
@@ -9,21 +9,24 @@ if (isset($_POST['login'])) {
 
     $ambil = mysqli_query($koneksi, "SELECT * FROM user WHERE email = '$email'");
 
-    if (mysqli_num_rows($ambil) ===1) {
+    if (mysqli_num_rows($ambil) === 1) {
         $data = mysqli_fetch_assoc($ambil);
 
         if ($password_hash === $data['password']) {
-            header("location: ../dashboard.html");
+            // Memeriksa domain email
+            if (strpos($email, '@kementan.go.id') !== false) {
+                header("location: ../tanamanSaya.html");
+            } else {
+                header("location: ../dashboard.html");
+            }
             exit();
-        }
-        else {
+        } else {
             echo"<script>
                 alert('password yang anda masukkan salah');
                 window.location = '../login.php';
             </script>";
         }
-    }
-    else {
+    } else {
         echo"<script>
             alert('username yang anda masukkan salah');
             window.location = '../login.php';
